2aad071fa56e55178b0bfabdd420d77a187f59e4
honda-yusuke <honda-yusuke@dmm.com>
2020-11-22 14:30:17 +0900

    implement main

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "os"
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/yyh-gl/ofukuro/butler"
+	"github.com/yyh-gl/ofukuro/task"
+)
 
 func main() {
+	b := butler.CallButler()
+
+	humidityNotification := task.NewHumidityNotification()
+
+	ctx := context.Background()
+	b.AddTask(ctx, humidityNotification)
+
+	if err := b.StartWorking(ctx); err != nil {
+		log.Fatal(err)
+	}
 	os.Exit(0)
 }
